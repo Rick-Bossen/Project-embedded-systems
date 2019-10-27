a48abcc0eceb3c202848131838db556de28b69d1
rizza99 <44263376+rizza99@users.noreply.github.com>
2019-10-27 18:20:08 +0100

    de code + librays
    alle troep om de code die in arduino geschreven staat te laten werken in C

---
+++ afstand_sensor/ArduinoSketch1/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 27-10-2019 18:07:13
+ * Author : shand
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
